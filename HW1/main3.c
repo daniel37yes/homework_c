78159ee81fbb2a31cadd04d064457702d0f47cb4
Daniel Stepanin <hamster3790@gmail.com>
2025-04-25 11:28:42 +0300

    first commit

---
+++ HW1/main3.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+/*   *
+    ***
+   *****
+  *******
+   HHOHH
+   ZZZZZ */
+{
+	printf("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HHOHH\n");
+	printf(" ZZZZZ\n");
+	return 0;
+}
+
