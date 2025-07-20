ab968d87892b883640b883dbfa0b34fae51610a7
daniel37yes <hamster3790@gmail.com>
2025-07-21 02:17:37 +0300

    Add files via upload

--- HW8/E5.c
+++ HW8/E5.c
@@ -15,22 +15,25 @@
 
 
 #include <stdio.h>
- 
+
 int main() 
 {
-int i;
-int sum = 0;
-int arr[10];
-   
-    for (i = 1; i < 10; i++)
+    int sum = 0;
+    int arr[10];
+    
+    for (int i = 0; i < 10; i++)
     {
         scanf("%d", &arr[i]);
+    }
+    
+    for (int i = 0; i < 10; i++)
+    {
         if (arr[i] > 0)
         {  
-            sum+=arr[i];
+            sum += arr[i];
         }
-	}
+    }
+    
     printf("%d\n", sum);
     return 0;
 }
-
